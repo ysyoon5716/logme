837736e6208e0a4aaad9a9220ddfbbebd24af42e
ysyoon <ysyoon5716@gmail.com>
2025-05-09 23:02:38 +0900

    Fix timezone

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 const DATABASE_NAME = 'logme';
+const timezoneOffset = 9 * 60 * 60 * 1000;
 
 const supabase = createClient(
     import.meta.env.VITE_SUPABASE_URL,
@@ -23,23 +24,26 @@ async function createLog() {
 async function getLogs() {
     const today = new Date();
     const startCreatedAt = new Date(today.setHours(0, 0, 0, 0)).toISOString();
-    const endCreatedAt = new Date(today.setHours(23, 59, 59, 999)).toISOString();
+    const endCreatedAt = new Date(today.setHours(23, 59, 59, 999)).toISOString();      
     const { data, error } = await supabase
         .from(DATABASE_NAME)
         .select('*')
         .order('created_at', { ascending: false })
         .gte('created_at', startCreatedAt)
         .lte('created_at', endCreatedAt)
-    console.log(data);
+    
 
     const logsContainer = document.getElementById('logsContainer');
     logsContainer.innerHTML = '';
     data.forEach(log => {
-        const time = new Date(log.created_at).toISOString().split('.')[0].replace('T', ' ');
+        let time = new Date(log.created_at);
+        time = new Date(time.getTime() + timezoneOffset);
+        const formattedTime = time.toISOString().split('.')[0].replace('T', ' ');
+
         const logMessageComponent = document.createElement('div');
         logMessageComponent.innerHTML = `
             <div class="bg-white rounded-lg mt-4">
-                <p class="text-sm text-gray-500">${time}</p>
+                <p class="text-sm text-gray-500">${formattedTime}</p>
                 <p>${log.message}</p>
             </div>
         `;
